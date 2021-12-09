b4c8b8bb54177d7c5a5f74cd8252fa613c7a8e0d
Jannik Hiller <jannik.hiller@rwth-aachen.de>
2021-12-09 12:49:00 +0100

    day9 1/2

--- day02/getinput.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wd=$(pwd)
-day=$((10#${wd: -2}))
-sess='53616c7465645f5f710d150b5e5e3a36833d628c0367d539c7a2fd3efbc8ee514154865ccf483c7800ceb2662da68aac'
-curl https://adventofcode.com/2021/day/$day/input --cookie "session=$sess" > input
